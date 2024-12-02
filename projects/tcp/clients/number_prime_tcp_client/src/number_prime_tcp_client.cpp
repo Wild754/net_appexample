ca378c6f6382dfc8c9b69fa9cea6f80e17723748
Wild754 <andrewbaklan25@gmail.com>
2024-12-02 19:34:02 +0200

    first commit

--- projects/tcp/clients/number_prime_tcp_client/src/number_prime_tcp_client.cpp
+++ projects/tcp/clients/number_prime_tcp_client/src/number_prime_tcp_client.cpp
@@ -1,88 +1,103 @@
 #include "number_prime_tcp_client.h"
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib") // Для Windows
 
 void exit_handler();
-
 SOCKET client_socket;
-int main(int argc, char* argv[])
-{
-	atexit(common_exit_handler);
-	atexit(exit_handler);
-	short port;
-	char host[128] = "";
-	bool parse_cmd_result = parse_cmd(argc, argv, host, &port);
 
-	if (!parse_cmd_result || !host || !strlen(host))
-	{
-		printf("Invalid host or port. Usage %s -h host -p port\n", argv[0]);
-		return -1;
-	}
+int main(int argc, char* argv[]) {
+    atexit(common_exit_handler);
+    atexit(exit_handler);
 
-	common_init_handler();
+    short port;
+    char host[128] = "";
+    bool parse_cmd_result = parse_cmd(argc, argv, host, &port);
 
-	
-	client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (client_socket <= 0) {
-		error_msg("Can't create socket");
-		return -1;
-	}
+    if (!parse_cmd_result || !host || !strlen(host)) {
+        printf("Invalid host or port. Usage %s -h host -p port\n", argv[0]);
+        return -1;
+    }
 
-	sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port);
-	resolve_addr(host, &server_addr.sin_addr);
+    common_init_handler();
 
-	//Connect to the server
-	if (connect(client_socket, (sockaddr*)&server_addr, sizeof(sockaddr))) {
-		char err_msg[128] = "";
-		sprintf(err_msg, "Can't connect to the server %s:%d", host, port);
-		error_msg(err_msg);
-		return -1;
-	}
+    client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (client_socket <= 0) {
+        error_msg("Can't create socket");
+        return -1;
+    }
 
-	printf("Connection to the server %s:%d success\n", host, port);
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    resolve_addr(host, &server_addr.sin_addr);
 
-	char control = '\0';
+    // Підключення до сервера
+    if (connect(client_socket, (sockaddr*)&server_addr, sizeof(sockaddr))) {
+        char err_msg[128] = "";
+        sprintf(err_msg, "Can't connect to the server %s:%d", host, port);
+        error_msg(err_msg);
+        return -1;
+    }
 
-	do {
-		int r;
-		printf("%s", "Enter a number to check for its prime:");
-		if (scanf("%d", &r) != 1)
-		{
-			printf("Invalid input\n");
-			return -1;
-		}
+    printf("Connection to the server %s:%d success\n", host, port);
 
-		int sc = send(client_socket, (char*)&r, sizeof(r), 0);
-		if (sc <= 0) {
-			char err_msg[128] = "";
-			sprintf(err_msg, "Can't send data to the server %s:%d", host, port);
-			error_msg(err_msg);
-			return -1;
-		}
+    char control = '\0';
 
-		{
-			char resp[256] = "";
-			sc = recv(client_socket, resp, sizeof(resp), 0);
-			if (sc <= 0)
-			{
-				printf("Server is not able\n");
-				return -1;
-			}
-			printf("%s", resp);
-		}
+    do {
+        double a, b, c;
+        printf("%s", "Enter coefficient a: ");
+        if (scanf("%lf", &a) != 1) {
+            printf("Invalid input\n");
+            return -1;
+        }
+        printf("%s", "Enter coefficient b: ");
+        if (scanf("%lf", &b) != 1) {
+            printf("Invalid input\n");
+            return -1;
+        }
+        printf("%s", "Enter coefficient c: ");
+        if (scanf("%lf", &c) != 1) {
+            printf("Invalid input\n");
+            return -1;
+        }
 
-		printf("\nDo you want to break? (Y/Any):");
-		
-		getchar();
-		control = getchar();		
-	} while (toupper(control) != 'Y');
+        // Відправка коефіцієнтів на сервер
+        double coefficients[3] = { a, b, c };
+        int sc = send(client_socket, (char*)coefficients, sizeof(coefficients), 0);
+        if (sc <= 0) {
+            char err_msg[128] = "";
+            sprintf(err_msg, "Can't send data to the server %s:%d", host, port);
+            error_msg(err_msg);
+            return -1;
+        }
 
-	printf("Bye!\n");
+        // Отримання відповіді від сервера
+        char resp[256] = "";
+        sc = recv(client_socket, resp, sizeof(resp), 0);
+        if (sc <= 0) {
+            printf("Server is not able\n");
+            return -1;
+        }
 
-	return 0;
+        printf("Server response: %s\n", resp);
+
+        // Запит на повторення
+        printf("\nDo you want to break? (Y/Any): ");
+        getchar();  // щоб зчитати зайвий символ нового рядка
+        control = getchar();
+
+    } while (toupper(control) != 'Y');
+
+    printf("Bye!\n");
+
+    return 0;
 }
 
-void exit_handler()
-{
+void exit_handler() {
     close_socket(client_socket);
 }
--- projects/tcp/servers/number_prime_tcp_server/src/number_prime_tcp_server.cpp
+++ projects/tcp/servers/number_prime_tcp_server/src/number_prime_tcp_server.cpp
@@ -1,36 +1,58 @@
 #include "number_prime_tcp_server.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")  // Для Windows
+
+// Функція для обчислення коренів квадратного рівняння
+void solve_quadratic(double a, double b, double c, char* result) {
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        double x1 = (-b + sqrt(discriminant)) / (2 * a);
+        double x2 = (-b - sqrt(discriminant)) / (2 * a);
+        sprintf(result, "Two roots: x1 = %.2f, x2 = %.2f", x1, x2);
+    }
+    else if (discriminant == 0) {
+        double x = -b / (2 * a);
+        sprintf(result, "One root: x = %.2f", x);
+    }
+    else {
+        sprintf(result, "No real roots");
+    }
+}
 
 void handle_connection(SOCKET socket, sockaddr_in* addr) {
-	char* str_in_addr = inet_ntoa(addr->sin_addr);
-	printf("[%s]>>%s\n", str_in_addr, "Establish new connection");
-	while (true) {
-		int r = 0;
-		int rc = recv(socket, (char*)&r, sizeof(r), 0);
-		if (rc > 0) {
-			printf("[%s]:Received number %d\n", str_in_addr, r);
-			char resp[256] = "";
-			sprintf(resp, "Number %d is %s", r, is_prime(r) ? "prime" : "compaund");
-			rc = send(socket, resp, strlen(resp), 0);
-			if (rc <= 0)
-			{
-				printf("Can't send response to client %s", str_in_addr);
-				break;
-			}
-		}
-		else {
-			break;
-		}
-	}
-	close_socket(socket);
-	printf("[%s]>>%s", str_in_addr, "Close incomming connection");
+    char* str_in_addr = inet_ntoa(addr->sin_addr);
+    printf("[%s]>>%s\n", str_in_addr, "Established new connection");
+
+    while (1) {
+        double coefficients[3]; // Для зберігання коефіцієнтів a, b, c
+        int rc = recv(socket, (char*)coefficients, sizeof(coefficients), 0); // Отримання коефіцієнтів
+
+        if (rc > 0) {
+            printf("[%s]: Received coefficients: a = %.2f, b = %.2f, c = %.2f\n",
+                str_in_addr, coefficients[0], coefficients[1], coefficients[2]);
+
+            char response[256] = "";
+            solve_quadratic(coefficients[0], coefficients[1], coefficients[2], response); // Обчислення розв'язку
+
+            // Надсилання результату клієнту
+            rc = send(socket, response, strlen(response), 0);
+            if (rc <= 0) {
+                printf("Can't send response to client %s\n", str_in_addr);
+                break;
+            }
+        }
+        else {
+            break;
+        }
+    }
+
+    closesocket(socket); // Закриття сокета
+    printf("[%s]>>%s\n", str_in_addr, "Closed incoming connection");
 }
 
-bool is_prime(int n)
-{
-	for (int i = 2; i < n; ++i)
-	{
-		if (n % i == 0)
-			return false;
-	}
-	return true;
-}
